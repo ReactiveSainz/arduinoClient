106485fb936f2b611dc7630e9198cd30b665466d
Luis ivan Sainz romero <thesainz94@gmail.com>
2018-12-04 17:48:37 -0600

    added stuff

--- src/App.js
+++ src/App.js
@@ -1,9 +1,64 @@
 import React, { Component } from "react";
 import "./App.css";
-
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend
+} from "recharts";
 class App extends Component {
+  state = {
+    data: []
+  };
+
+  componentDidMount() {
+    this.fecthData();
+  }
+
+  fecthData = () => {
+    fetch("https://arduinoserverfrank.herokuapp.com/api/registros", {
+      method: "GET"
+    })
+      .then(res => res.json())
+      .then(res => {
+        console.log("res", res);
+        this.setState({ data: res });
+      });
+  };
+
   render() {
-    return <div className="App">fsdfsfsd</div>;
+    const { data } = this.state;
+    return (
+      <div className="App">
+        <LineChart
+          width={600}
+          height={300}
+          data={data}
+          margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+        >
+          <XAxis dataKey="date" />
+          <YAxis />
+          <CartesianGrid strokeDasharray="3 3" />
+          <Tooltip />
+          <Legend />
+          <Line
+            type="monotone"
+            dataKey="humedad"
+            stroke="#8884d8"
+            activeDot={{ r: 8 }}
+          />
+          <Line
+            type="monotone"
+            dataKey="temperatura"
+            stroke="#82ca9d"
+            activeDot={{ r: 8 }}
+          />
+        </LineChart>
+      </div>
+    );
   }
 }
 
